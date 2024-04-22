d5a3eccbf186453f7a08c2409f43bdba39ab0863
ashMatch <bruno.rochah95@gmail.com>
2024-04-22 15:07:25 -0300

    Added class for area and the right math for each area

--- AreaMath.py
+++ AreaMath.py
@@ -1,12 +1,13 @@
 class AreaMath:
-    def __init__(self, base, high, bigger_base = 0, radius = 0, PI = 3.14):
+
+    def __init__(self, base = 0, high = 0, bigger_base = 0, radius = 0, PI = 3.14):
         self.base = base
         self.high = high
-        self.radius =radius
         self.bigger_base = bigger_base
+        self.radius =radius
+        self.PI = PI
     
     def triangle_math(self):
-        print("A = BasexAltura/2²")
         area =self.base*self.high/2
         return "A = %.1f" %(area)
     
@@ -15,9 +16,13 @@ class AreaMath:
         return "A = %.1f" %(area)
     
     def square_math(self):
-        area =self.base*self.high
+        area =self.base*self.base
         return "A = %.1f" %(area)
     
     def circle_math(self):
-        area =self.base*self.high
+        area = self.PI*self.radius**2
         return "A = %.1f" %(area)
+    
+    def trapezoid_math(self):
+        area = (self.base+self.bigger_base/2)*self.high
+        return "A = %.1f" % (area)
--- area-figure.py
+++ area-figure.py
@@ -1,7 +1,6 @@
 from AreaMath import AreaMath
 from type_area import type_area
 
-area_math = AreaMath(4,5)
 
 print("Esse programa calcula a area de uma figura plana")
 game_on = input("você quer jogar?[y]yes/[n]no: ")
@@ -21,22 +20,39 @@ else:
     if user_choice == 1:
         print("Area do quadrado")
         print("\nA = L²\n")
-        print(area_math.square_math())
+        base = int(input("Informe a base: "))
+        square_area = AreaMath(base)
+        print(square_area.square_math())
     elif user_choice == 2:
         print("Area do Retangulo")
         print("\nA = Base x Altura\n")
-        print(area_math.rectangle_math())
+        base = int(input("Informe a base: "))
+        hight = int(input("Informe a altura: "))
+        rectangle_area = AreaMath(base, hight)
+        print(rectangle_area.rectangle_math())
+        ##print(area_math.rectangle_math())
     elif user_choice ==3:
         print("Area do Triangulo")
         print("\nresultado igual:")
-        print(area_math.triangle_math())
+        base = int(input("Informe a base: "))
+        hight = int(input("Informe a altura: "))
+        triangle_area = AreaMath(base, hight)
+        print(triangle_area.triangle_math())
     elif user_choice == 4:
         print("Area da Circunferencia")
         print("\nA = PI x R²\n")
+        radius = int(input("Informe o raio da circunferencia: "))
+        circle_area = AreaMath(0, 0, 0, radius)
+        print(circle_area.circle_math())
 
     elif user_choice == 5:
         print("Area da Trapezoid")
         print("\nA = (Base Maior + Base Menor/2) x h\n")
-    
+        base = int(input("Informe a base menor: "))
+        bigger_base = int(input("Informe a base maior: "))
+        hight = int(input("Informe a altura: "))
+        trapezoid_area = AreaMath(base, hight, bigger_base)
+        print(trapezoid_area.trapezoid_math())
+
     ## print(type(user_choice))
     ## print(type_area.type_area(int(user_choice)))
